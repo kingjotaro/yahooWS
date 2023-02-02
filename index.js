6e4e78e1e3fc2ee7ae88061a17cafa296bd34ae3
Rafael Lourenço <rafaelleet@gmail.com>
2023-02-02 10:08:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const inputDate = "Mar 10, 1998"; /* input a ser dado dinamicamente */
 const { TimeoutError } = require('puppeteer');
 (async() => { 
     /* await pupt.launch espera o navegador do puppeteer iniciar, headless false mostra o navegador */
-    const browser = await pupt.launch();
+    const browser = await pupt.launch({ args: ['--no-sandbox'] });
 
     /* abri uma nova aba */
     const page = await browser.newPage();
